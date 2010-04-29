2f996d93ffafecc7810b89c691709a8392d3f420
jaraco <none@none>
2010-04-29 15:45:30 +0000

    More cleanup of setup file

--- setup.py
+++ setup.py
@@ -7,26 +7,17 @@ setup(
 	name='jaraco.excel',
 	version=version,
 	description="Utility library for working with MS Excel documents",
-	long_description="""\
-""",
 	keywords='excel',
 	author='Jason R. Coombs',
 	author_email='jaraco@jaraco.com',
-	url='http://www.jaraco.com',
+	url='http://pypi.python.org/pypi/jaraco.excel',
 	license='MIT',
 	packages=find_packages(),
-	namespace_packages = ['jaraco']
+	namespace_packages = ['jaraco'],
 	classifiers = [
 		"Development Status :: 4 - Beta",
 		"Intended Audience :: Developers",
 		"Programming Language :: Python",
 	],
-	include_package_data=True,
 	zip_safe=True,
-	install_requires=[
-	  # -*- Extra requirements: -*-
-	],
-	entry_points="""
-	# -*- Entry points: -*-
-	""",
 	)
