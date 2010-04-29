feb1ac82b70d3e4dbb3853063eb2efa0bb8bc813
jaraco <none@none>
2010-04-29 15:43:19 +0000

    Began cleaning up setup routine

--- setup.py
+++ setup.py
@@ -9,14 +9,13 @@ setup(
 	description="Utility library for working with MS Excel documents",
 	long_description="""\
 """,
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	keywords='excel',
 	author='Jason R. Coombs',
 	author_email='jaraco@jaraco.com',
 	url='http://www.jaraco.com',
 	license='MIT',
-	packages=find_packages('src'),
-	package_dir = {'': 'src'},
+	packages=find_packages(),
+	namespace_packages = ['jaraco']
 	classifiers = [
 		"Development Status :: 4 - Beta",
 		"Intended Audience :: Developers",
