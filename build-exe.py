ec53998b580045a01f5edd2a31a3874f5d650f9c
Jason R. Coombs <jaraco@jaraco.com>
2017-12-18 12:37:49 -0500

    Remove workaround for mime names, which breaks Python 3 tests.

--- build-exe.py
+++ build-exe.py
@@ -5,17 +5,12 @@ Build script to create a doc-to-pdf convert server as a Windows executable.
 import os
 import textwrap
 
-# due to the funny way that email.mime does its imports, it doesn't get
-#  picked up. CherryPy needs these to be present to not throw errors in
-#  the autoreloader.
-import email
-mime_names = ['email.mime.' + name.lower() for name in email._MIMENAMES]
 
 setup_params = dict(
 	console=['server.py'],
 	options=dict(
 		py2exe=dict(
-			packages=['pkg_resources'] + mime_names,
+			packages=['pkg_resources'],
 		),
 	),
 	script_args=('py2exe',),
